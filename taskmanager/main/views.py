35e879b6288bedfcc906f0c7659522348f6ed567
AndriiLevin <andriilevin@gmail.com>
2023-06-27 19:29:26 +0100

    add Task to databae

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from .models import Task
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Main paGGGeee', 'tasks': tasks})
 
 
 def about(request):
